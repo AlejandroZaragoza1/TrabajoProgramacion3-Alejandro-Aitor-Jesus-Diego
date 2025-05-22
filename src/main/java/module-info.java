40be8cb16f0dc966551d30e919aa36cfd8b5a978
Jesus Cobo <146002181+JesusCoboArrogante@users.noreply.github.com>
2025-05-22 15:54:16 +0200

    greadlew y carpeta de test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module dev.alejandroaitorjesusdiego.proyectoprogramacion3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires kotlin.stdlib;
+    requires logging.jvm;
+    requires org.jdbi.v3.core;
 
 
     opens dev.alejandroaitorjesusdiego.proyectoprogramacion3 to javafx.fxml;
