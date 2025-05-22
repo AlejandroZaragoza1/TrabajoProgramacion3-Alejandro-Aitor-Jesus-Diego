e04a3477a3733475b2fcee104e8c2d4e8a5680d7
ByDiegox78 <oms04081974@gmail.com>
2025-05-22 20:38:32 +0200

    Models y DTO

---
+++ src/main/kotlin/dev/alejandroaitorjesusdiego/proyectoprogramacion3/models/Integrante.kt
@@ -0,0 +1,35 @@
+package org.example.newteam.gestion.models
+
+import dev.alejandroaitorjesusdiego.proyectoprogramacion3.models.Tipo
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+/**
+ * Clase abstracta que representa a un integrante de un equipo de futbol. Superclase de [Jugador] y [Entrenador]
+ * @param id [Long] Identificador
+ * @param nombre [String] Nombre
+ * @param apellidos [String] Apellidos
+ * @param fecha_nacimiento [LocalDate] Fecha de nacimiento
+ * @param fecha_incorporacion [LocalDate] Fecha de incorporacion al equipo
+ * @param pais [String] Pais de origen
+ * @param createdAt [LocalDateTime] Fecha y hora a la que se creo el objeto
+ * @param updatedAt [LocalDateTime] Fecha y hora a la que se actualizo el objeto por ultima vez
+ * @param imagen [String] Imagen de perfil del integrante
+ * @property nombreCompleto Campo calculado que fusiona los apellidos y el nombre
+ */
+abstract class Integrante (
+    val id: Long = 0L,
+    var nombre: String,
+    val apellidos: String,
+    val fecha_nacimiento: LocalDate,
+    val fecha_incorporacion: LocalDate, //Localizar
+    val salario: Double,
+    val pais: String,
+    val tipo: Tipo,
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+    var updatedAt: LocalDateTime = LocalDateTime.now(),
+    var imagen: String = "resources/org/example/newteam/media/profile_picture.png"
+){
+    val nombreCompleto: String
+            get() = "$apellidos, $nombre"
+}
